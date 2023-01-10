b0b2b3764d3a012555e98e54f563279ea6387f79
4quatic <4quatic@gmx.net>
2023-01-10 15:37:11 +0100

    Map3 und map3.js hinzugefügt.

---
+++ map3.js
@@ -0,0 +1,37 @@
+import { config } from "./config.js"
+import { playercontrol } from "./control.js"
+import { createMap } from "./initmap.js"
+import { createPlayer } from "./player.js"
+import { checkCollisions } from "./collisionhandler.js"
+
+class Map2 {
+    create(){
+        createMap('map3', this)
+        
+        createPlayer(this)
+        console.log(config.linkstatus)
+
+        this.matter.world.setBounds(0, 0, config.map.widthInPixels, config.map.heightInPixels);
+        config.cursors = this.input.keyboard.createCursorKeys();
+
+        config.camera = this.cameras.main;
+        config.camera.setBounds(0,0, config.map.widthInPixels, config.map.heightInPixels)
+        config.camera.startFollow(config.link, true, 0.05, 0.05)
+
+        config.fog = this.add.tileSprite(0, 0, config.map.widthInPixels, config.map.heightInPixels, 'fog').setOrigin(0,0);
+        config.fog.setDepth(100);
+
+        checkCollisions(this)
+
+    }
+    update(){
+
+        playercontrol()
+
+        config.fog.x = config.camera.scrollX*0.2;
+        config.fog.y = config.camera.scrollY*0.2;
+
+    }
+}
+
+export { Map3 }
